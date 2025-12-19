f7b07eb7a79f66902af8198ef2605454a079c8f2
Kingsley Somuah <kingsleysomuah123@gmail.com>
2025-12-19 05:03:08 +0000

    changes made to settings

--- Blog/settings.py
+++ Blog/settings.py
@@ -9,11 +9,16 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# SECURITY
+SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret-key")
+DEBUG = os.environ.get("DEBUG", "FALSE") == "TRUE"
+ALLOWED_HOSTS = os.environ.get("ALLOWED HOSTS", "*").split(",")
 
 
 # Quick-start development settings - unsuitable for production
@@ -77,10 +82,7 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
 }
 
 
